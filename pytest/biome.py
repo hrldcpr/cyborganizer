1e591df93b32ba61da2a3a6694356ff10dbe1fac
Harold Cooper <hcooper@ebay.com>
2012-05-31 11:51:33 -0400

    desert and jungle

--- pytest/biome.py
+++ pytest/biome.py
@@ -8,12 +8,15 @@ FOREST = (0, 128, 0)
 LAKE = (0, 128, 128)
 MOUNTAIN = (64, 64, 64)
 SNOW = (224, 224, 255)
+DESERT = (192, 192, 0)
 
-BIOMES = [OCEAN, MOUNTAIN, SNOW]
+BIOMES = [OCEAN, MOUNTAIN, SNOW, JUNGLE, DESERT]
 
 _IMAGES = {OCEAN: pygame.image.load('images/water.1.medium.png'),
            MOUNTAIN: pygame.image.load('images/mountain.1.medium.png'),
            SNOW: pygame.image.load('images/snow.1.medium.png'),
+           JUNGLE: pygame.image.load('images/jungle.1.medium.png'),
+           DESERT: pygame.image.load('images/desert.2.medium.png'),
            }
 
 IMAGES = {}
