9efa74f3b31534be56006d84b61fea2b03514a77
Harold Cooper <hrldcpr@gmail.com>
2013-03-09 13:34:49 -0500

    prettify

--- uitest/index.js
+++ uitest/index.js
@@ -1,46 +1,51 @@
-$(function() {
-//////////////
+(function(window, document, undefined) {
+//************************************//
 
 
-function createSVG(tag) {
+function svg(tag) {
     return $(document.createElementNS('http://www.w3.org/2000/svg', tag));
 }
 
-var deepest = 1;
+var MAX_ZOOM = 100, MIN_ZOOM = -MAX_ZOOM;
+var deepest = 0;
 function zoom(scale, fleeting) {
-    var scale0 = $('#world').data('scale') || 1;
-    scale *= scale0;
+    var world = $('#world');
+    scale *= world.data('scale') || 1;
+    var zoom = Math.log(scale) / Math.LN2;
 
-    $('#world').attr('transform', 'scale(' + scale + ',' + scale + ')');
-    if (!fleeting) $('#world').data('scale', scale)
+    if (zoom < MIN_ZOOM || zoom > MAX_ZOOM) return;
 
-    while (scale >= deepest) {
-        deepest *= 2;
-        createSVG('rect')
-            .attr('width', 1 / deepest)
-            .attr('height', 1 / deepest)
-            .attr('fill', 'rgb(' + Math.floor(256*Math.random())
-                  + ',' + Math.floor(256*Math.random())
-                  + ',' + Math.floor(256*Math.random()) +')')
-            .appendTo('#world');
+    world.attr('transform', 'scale(' + scale + ',' + scale + ')');
+    if (!fleeting) world.data('scale', scale)
+
+    while (deepest <= zoom + 2) {
+        svg('rect')
+            .attr('width', 1 / Math.pow(2, deepest))
+            .attr('height', 1 / Math.pow(2, deepest))
+            .attr('fill', deepest % 2 ? 'white' : 'red')
+            .appendTo(world);
+        deepest++;
     }
 }
 
-zoom(1);
 
-$('svg').on('mousewheel', function(event, delta) {
-    event.preventDefault();
-    zoom(Math.pow(1.01, delta));
+$(function() {
+
+    zoom(1);
+
+    $('svg').on('mousewheel', function(event, delta) {
+        event.preventDefault();
+        zoom(Math.pow(1.01, delta));
+    }).hammer({
+        transform_always_block: true
+    }).on('transform', function(event) {
+        zoom(event.gesture.scale, true);
+    }).on('transformend', function(event) {
+        zoom(event.gesture.scale, false);
+    });
+
 });
 
-$('svg').hammer({
-    transform_always_block: true
-}).on('transform', function(event) {
-    zoom(event.gesture.scale, true);
-}).on('transformend', function(event) {
-    zoom(event.gesture.scale, false);
-});
 
-
-///
-});
+//********************//
+}(this, this.document));
